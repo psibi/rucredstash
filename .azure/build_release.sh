1700b06ddb92bfd7e307abeaa2c200b57ecc6c79
Sibi Prabakaran <sibi@psibi.in>
2019-12-29 16:46:59 +0530

    Build in release mode

--- .azure/build_release.sh
+++ .azure/build_release.sh
@@ -2,9 +2,13 @@
 
 set -eux
 
+cargo build --release
+
 rm -rf "${BUILD_BINARIESDIRECTORY}"
 mkdir "${BUILD_BINARIESDIRECTORY}"
 
-cargo install --path "${BUILD_BINARIESDIRECTORY}/" --force
-
-ls "${BUILD_BINARIESDIRECTORY}/"
+if [[ -f "target/release/credstash.exe" ]]; then
+  mv "target/release/credstash.exe" 
+else
+  mv "target/release/credstash" "${BUILD_BINARIESDIRECTORY}/"
+fi
