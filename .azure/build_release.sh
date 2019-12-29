fca9a594305693569a57579733b8a62a03fb089e
Sibi Prabakaran <sibi@psibi.in>
2019-12-29 17:23:31 +0530

    Further debug

--- .azure/build_release.sh
+++ .azure/build_release.sh
@@ -2,8 +2,6 @@
 
 set -eux
 
-
-
 rm -rf "${BUILD_BINARIESDIRECTORY}"
 mkdir "${BUILD_BINARIESDIRECTORY}"
 
@@ -11,8 +9,10 @@ ls .
 
 cargo build --release
 
-if [[ -f "target/release/credstash.exe" ]]; then
-  mv "target/release/credstash.exe" 
+ls -R
+
+if [[ -f "target/release/rucredstash.exe" ]]; then
+  mv "target/release/rucredstash.exe" 
 else
-  mv "target/release/credstash" "${BUILD_BINARIESDIRECTORY}/"
+  mv "target/release/rucredstash" "${BUILD_BINARIESDIRECTORY}/"
 fi
