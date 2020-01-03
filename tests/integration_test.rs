67cdd7432416803ace8cc645509690bbba1f9672
Sibi Prabakaran <sibi@psibi.in>
2020-01-03 21:20:34 +0530

    Write tests for other exposed functions

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -34,6 +34,29 @@ fn credstash_basic_workflow() {
     let secret_utf8 = str::from_utf8(&result.credential_value).unwrap();
     assert_eq!("world12".to_string(), secret_utf8);
 
+    // put auto increment
+    let put_auto_secret_future = app.put_secret_auto_version(
+        table_name.clone(),
+        "hello12".to_string(),
+        "world12".to_string(),
+        None,
+        None,
+        None,
+        ring::hmac::HMAC_SHA256,
+    );
+    let result = core.run(put_auto_secret_future);
+    assert!(result.is_ok());
+
+    // get version
+    let get_version_future = app.get_highest_version(table_name.clone(), "hello12".to_string());
+    let result = core.run(get_version_future);
+    assert_eq!(result.unwrap(), 2);
+
+    // get all secrets
+    let get_all_future = app.get_all_secrets(table_name.clone(), None, None);
+    let result = core.run(get_all_future);
+    assert_eq!(result.unwrap().len() >= 1, true);
+
     // delete credential
     let delete_future = app.delete_secret(table_name, "hello12".to_string());
     let res = core.run(delete_future);
